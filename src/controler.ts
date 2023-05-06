08a76077d17b8b70685c1f4cb88068522171e885
Artem <zinkovskiyy87@gmail.com>
2023-05-06 17:21:18 +0300

    fixed shifts

--- src/controler.ts
+++ src/controler.ts
@@ -4,9 +4,8 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-var-requires */
 const { GoogleSpreadsheet } = require('google-spreadsheet');
-import { error } from 'console';
 import { regParse } from './model/model';
-import { writeToFile } from './addToFile';
+import { Shifts } from './model/ShiftsType';
 
 require('dotenv').config();
 
@@ -17,18 +16,12 @@ const regExType: RegExp = /доп|отработка|отработал/;
 const regExSince: RegExp = /\b\d\d:\d\d\b|\b\d:\d\d\b/;//(d{2}):(d{2})/;
 const regExDay: RegExp = /\b\d\d.\d\d|\b\d.\d\d/;
 
-let data = {
-    date: '',
-    ldap: '',
-    time_since: '',
-    time_to: '',
-    type: '',
-    for_date: ''
-};
 
 const doc = new GoogleSpreadsheet('1YUs8Ye_8jTbEYeO_HoDvKuOPmqFCTacawNTb9SAVFx0');//в аргументах ИД документа
 
 const start = async (msg: any): Promise<void> => {
+
+    if (msg === undefined) throw console.log('msg from user in undefined!')
     let str: string = msg.text!;
     str = str.toLowerCase();
 
@@ -42,53 +35,53 @@ const start = async (msg: any): Promise<void> => {
     await doc.loadInfo(); // loads document properties and worksheets
     const sheet = await doc.sheetsByIndex[0];
 
-
-    data.ldap = msg.from.username;
-    console.log(data.ldap);
-    data.type = regParse.getRegEx(str, regExType);
     // ищет регулярное выражение в сообжении
-    if (regParse.matchRegEx(str, regExSince)) {
-        data.time_since = regParse.getRegEx(str, regExSince);
-        str = regParse.replaceSubStr(str, data.time_since);
+    if (regParse.matchRegEx(str, regExType)) {
+        //добавление собраные данные в гугл таблицу
+        let data: Shifts[] = collectData(msg)
+        for (let item of data) {
+            await sheet.addRow(item);
+        }
+    }
+    else console.log('Not found regex');
+}
 
-        data.time_to = regParse.getRegEx(str, regExSince);
-        str = regParse.replaceSubStr(str, data.time_to);
+
+const collectData = (msg: any): Shifts[] => {
+    let data: Shifts[] = [];
+    let date: string, ldap: string, time_since: string, time_to: string, type: string, for_date: string;
+    let str: string = msg.text!;
+    str = str.toLowerCase();
+
+    type = regParse.getRegEx(str, regExType);
+    console.log(msg.from.id);
+    console.log(msg.from.username);
+
+    ldap = msg.from.username;
+
+    do {
+        time_since = regParse.getRegEx(str, regExSince);
+        str = regParse.replaceSubStr(str, time_since);
+
+        time_to = regParse.getRegEx(str, regExSince);
+        str = regParse.replaceSubStr(str, time_to);
 
         let dataTime = new Date();
         let day = dataTime.getMonth()
         let currentData = `${dataTime.getDate()}.${day < 10 ? '0' + day : day}.${dataTime.getFullYear()}`;
-        data.date = currentData;
+        date = currentData;
         //проверяет отработка или допка(доп смены отсутствует поле "за какую дату")
-        if (data.type !== 'доп') {
+        if (type !== 'доп') {
             let res;
-            data.type = 'отработка';
+            type = 'отработка';
             res = regParse.getRegEx(str, regExDay);
-            console.log(res);
-            data.for_date = res.length == 1 ? '0' + res : res;
+            str = regParse.replaceSubStr(str, res);
+            for_date = res.length == 1 ? '0' + res : res;
         }
-        //добавление собраные данные в гугл таблицу
-        await sheet.addRow(data);
-        console.log(data);
-        data.for_date = '';
-    }
-    else console.log('Not found regex');
-
-    try {
-        //const rows = await sheet.getRows(); // can pass in { limit, offset }
-
-        // read/write row values
-        // writeToFile(rows.map((value: { _rawData: any; })=>{
-        //     return value._rawData;
-        // }));
-        //console.log(rows[rows.length - 1]);
-    }
-    catch (e) {
-        console.error(`output ${error}`)
-    }
-    //console.log(sheet.getSheet());
-    //console.log(cel);
-    //console.log(await sheet.values.get('1YUs8Ye_8jTbEYeO_HoDvKuOPmqFCTacawNTb9SAVFx0','A1:C3'));
-    
+        else for_date = '';
+        data.push({ ldap, date, time_since, time_to, type, for_date });
+    } while (regParse.matchRegEx(str, regExSince))
+    return data;
 }
 
 
---
+++ src/model/ShiftsType.ts
@@ -0,0 +1,8 @@
+export type Shifts = {
+    date: string,
+    ldap: string,
+    time_since: string,
+    time_to: string,
+    type: string,
+    for_date: string
+ }
